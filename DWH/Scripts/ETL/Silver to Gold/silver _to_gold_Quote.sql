e278e87e090952793d931a79f34093073859dee3
Woojek1 <wojciklukaszk@gmail.com>
2025-05-14 15:51:15 +0200

    Add view to gold schema

---
+++ DWH/Scripts/ETL/Silver to Gold/silver _to_gold_Quote.sql
@@ -0,0 +1,74 @@
+select * from silver.bc_sales_lines_zymetric
+
+-------------------------------------------------
+
+CREATE OR REPLACE VIEW gold.v_Oferty AS
+WITH Oferty_Aircon AS (
+	SELECT
+		CONCAT(sl."Firma", '_', sl."documentNo") as "Klucz oferty"
+		,sl."documentType"
+		,sl."documentNo" as "Numer oferty"
+		,sl."lineNo" as "Linia oferty"
+		,sl."shortcutDimension2Code" as "Nr projektu"
+		,CONCAT(sl."Firma", '_', sl."shortcutDimension2Code") as "Klucz projektu"
+		,qh."Document_Date" as "Data dokumentu"
+		,sl."no" as "Symbol urzadzenia"
+		,sl."lineAmount" as "Wartosc PLN"
+		,sl."ednCoolingCapacityKW" as "Moc chłodnicza"	
+	FROM
+		silver.bc_sales_lines_aircon sl
+	INNER JOIN
+		silver.bc_sales_quotes_header_aircon qh
+	ON 
+		sl."documentNo" = qh."No"
+),
+
+Oferty_Technab AS
+	SELECT
+		CONCAT(sl."Firma", '_', sl."documentNo") as "Klucz oferty"
+		,sl."documentType"
+		,sl."documentNo" as "Numer oferty"
+		,sl."lineNo" as "Linia oferty"
+		,sl."shortcutDimension2Code" as "Nr projektu"
+		,CONCAT(sl."Firma", '_', sl."shortcutDimension2Code") as "Klucz projektu"
+		,qh."Document_Date" as "Data dokumentu"
+		,sl."no" as "Symbol urzadzenia"
+		,sl."lineAmount" as "Wartosc PLN"
+		,sl."ednCoolingCapacityKW" as "Moc chłodnicza"	
+	FROM
+		silver.bc_sales_lines_technab sl
+	INNER JOIN
+		silver.bc_sales_quotes_header_technab qh
+	ON 
+		sl."documentNo" = qh."No"
+),
+
+Oferty_Zymetric AS
+	SELECT
+		CONCAT(sl."Firma", '_', sl."documentNo") as "Klucz oferty"
+		,sl."documentType"
+		,sl."documentNo" as "Numer oferty"
+		,sl."lineNo" as "Linia oferty"
+		,sl."shortcutDimension2Code" as "Nr projektu"
+		,CONCAT(sl."Firma", '_', sl."shortcutDimension2Code") as "Klucz projektu"
+		,qh."Document_Date" as "Data dokumentu"
+		,sl."no" as "Symbol urzadzenia"
+		,sl."lineAmount" as "Wartosc PLN"
+		,sl."ednCoolingCapacityKW" as "Moc chłodnicza"	
+	FROM
+		silver.bc_sales_lines_technab sl
+	INNER JOIN
+		silver.bc_sales_quotes_header_technab qh
+	ON 
+		sl."documentNo" = qh."No"
+)
+
+SELECT *
+	FROM Oferty_Aircon
+UNION ALL
+SELECT *
+	FROM Oferty_Technab
+UNION ALL
+SELECT *
+	FROM Oferty_Zymetric
+;
---
+++ DWH/Scripts/ETL/Silver to Gold/silver_to_gold_Projects.sql
@@ -0,0 +1,77 @@
+select * from silver.bc_projects_aircon
+
+-------------------------------------------------
+
+create or replace view gold.v_Projekty AS
+with Projekty_Aircon as (
+	select
+		concat("Firma", '_', "No") as "Klucz projektu"
+		,"No" as "Numer projektu"
+		,"Description" as "Nazwa projektu"
+		,"Description_2" as "Etap projektu"
+		,"Status" as "Status"
+		,"Creation_Date" as "Data utworzenia"
+		,"Manufacturer_Code" as "Kod Producenta"
+		,"City" as "Miasto"
+		,"County" as "Wojewodztwo"
+		,"Object_Type" as "Typ obiektu"
+		,"Project_Source" as "Zrodlo projektu"
+		,"Manufacturer" as "Producent"
+		,"Planned_Delivery_Date" as "Planowana data dostawy"
+		,"Project_Account_Manager" as "Account Manager"
+		,"Salesperson_Code" as "Kod sprzedawcy"
+	from
+		silver.bc_projects_aircon
+),
+
+Projekty_Technab as (
+	select
+		concat("Firma", '_', "No") as "Klucz projektu"
+		,"No" as "Numer projektu"
+		,"Description" as "Nazwa projektu"
+		,"Description_2" as "Etap projektu"
+		,"Status" as "Status"
+		,"Creation_Date" as "Data utworzenia"
+		,"Manufacturer_Code" as "Kod Producenta"
+		,"City" as "Miasto"
+		,"County" as "Wojewodztwo"
+		,"Object_Type" as "Typ obiektu"
+		,"Project_Source" as "Zrodlo projektu"
+		,"Manufacturer" as "Producent"
+		,"Planned_Delivery_Date" as "Planowana data dostawy"
+		,"Project_Account_Manager" as "Account Manager"
+		,"Salesperson_Code" as "Kod sprzedawcy"
+	from
+		silver.bc_projects_technab
+),
+
+Projekty_Zymetric as (
+	select
+		concat("Firma", '_', "No") as "Klucz projektu"
+		,"No" as "Numer projektu"
+		,"Description" as "Nazwa projektu"
+		,"Description_2" as "Etap projektu"
+		,"Status" as "Status"
+		,"Creation_Date" as "Data utworzenia"
+		,"Manufacturer_Code" as "Kod Producenta"
+		,"City" as "Miasto"
+		,"County" as "Wojewodztwo"
+		,"Object_Type" as "Typ obiektu"
+		,"Project_Source" as "Zrodlo projektu"
+		,"Manufacturer" as "Producent"
+		,"Planned_Delivery_Date" as "Planowana data dostawy"
+		,"Project_Account_Manager" as "Account Manager"
+		,"Salesperson_Code" as "Kod sprzedawcy"
+	from
+		silver.bc_projects_zymetric
+)
+
+select *
+	from Projekty_Aircon
+union all
+select *
+	from Projekty_Technab
+union all
+select *
+	from Projekty_Zymetric
+;
