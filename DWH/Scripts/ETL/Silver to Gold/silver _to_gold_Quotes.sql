6b810e1f2c0ee2c18de310b2d64e4ac083230f47
Woojek1 <wojciklukaszk@gmail.com>
2025-05-21 14:26:19 +0200

    Calculation PLN value for offer in EUR

--- DWH/Scripts/ETL/Silver to Gold/silver _to_gold_Quotes.sql
+++ DWH/Scripts/ETL/Silver to Gold/silver _to_gold_Quotes.sql
@@ -1,22 +1,26 @@
 CREATE OR REPLACE VIEW gold.v_Oferty AS
 WITH Oferty_Aircon AS (
 	SELECT
-		CONCAT(sl."Firma", '_', sl."documentNo") AS "Klucz oferty"
-		,sl."documentType"
-		,sl."documentNo" AS "Numer oferty"
-		,sl."lineNo" AS "Linia oferty"
+		sl."documentNo" AS "Nr oferty"
+		,CONCAT(sl."Firma", '_', sl."documentNo") AS "Klucz oferty"
+		,sl."documentType" AS "Typ dokumentu"
 		,sl."shortcutDimension2Code" AS "Nr projektu"
 		,CONCAT(sl."Firma", '_', sl."shortcutDimension2Code") AS "Klucz projektu"
 		,qh."Document_Date" AS "Data dokumentu"
+		,sl."lineNo" AS "Linia oferty"
 		,sl."no" AS "Symbol urzadzenia"
-		,sl."lineAmount" AS "Wartosc PLN"
 		,sl."quantity" AS "Ilosc"
+		,sl."lineAmount" AS "Wartosc"
+		,sl."ednPriceListCurrencyCode" AS "Kod waluty"
+		,sl."ednPriceListExchangeRate" AS "Kurs wymiany"
+		,(sl."lineAmount" * sl."ednPriceListExchangeRate") AS "Wartosc PLN"
 --		,sl."ednOryUnitCostLCY" as "koszt"
 		,(sl."ednOryUnitCostLCY") * (sl."quantity") AS "Koszt urzadzen PLN"
-		,(sl."lineAmount") - ((sl."ednOryUnitCostLCY") * (sl."quantity")) AS "Zysk PLN"
-		,sl."ednCoolingCapacityKW" AS "Moc chłodnicza"
-		,CONCAT(qh."Firma", '_', qh."Sell_to_Customer_No") AS "Nr klienta"		-- Litera firmy dodana w elu utworzenia klucza klienta w każdej spółce
+		,((sl."lineAmount" * sl."ednPriceListExchangeRate") - (sl."ednOryUnitCostLCY") * (sl."quantity")) AS "Zysk PLN"
+		,sl."ednCoolingCapacityKW" AS "Moc chLodnicza"
+		,CONCAT(qh."Firma", '_', qh."Sell_to_Customer_No") AS "Klucz klienta"		-- Litera firmy dodana w elu utworzenia klucza klienta w każdej spółce
 		,qh."Sell_to_Customer_Name" AS "Nazwa klienta"
+--		,qh."VAT_Registration_No" AS "NIP"
 		,qh."Salesperson_Code" AS "Handlowiec"
 		,qh."Status" AS "Status oferty"
 		,qh."EDN_Quote_Type_Code" AS "Kod typu oferty"
@@ -31,22 +35,26 @@ WITH Oferty_Aircon AS (
 
 Oferty_Technab AS (
 	SELECT
-		CONCAT(sl."Firma", '_', sl."documentNo") AS "Klucz oferty"
-		,sl."documentType"
-		,sl."documentNo" AS "Numer oferty"
-		,sl."lineNo" AS "Linia oferty"
+		sl."documentNo" AS "Nr oferty"
+		,CONCAT(sl."Firma", '_', sl."documentNo") AS "Klucz oferty"
+		,sl."documentType" AS "Typ dokumentu"
 		,sl."shortcutDimension2Code" AS "Nr projektu"
 		,CONCAT(sl."Firma", '_', sl."shortcutDimension2Code") AS "Klucz projektu"
 		,qh."Document_Date" AS "Data dokumentu"
+		,sl."lineNo" AS "Linia oferty"
 		,sl."no" AS "Symbol urzadzenia"
-		,sl."lineAmount" AS "Wartosc PLN"
 		,sl."quantity" AS "Ilosc"
+		,sl."lineAmount" AS "Wartosc"
+		,sl."ednPriceListCurrencyCode" AS "Kod waluty"
+		,sl."ednPriceListExchangeRate" AS "Kurs wymiany"
+		,(sl."lineAmount" * sl."ednPriceListExchangeRate") AS "Wartosc PLN"
 --		,sl."ednOryUnitCostLCY" as "koszt"
 		,(sl."ednOryUnitCostLCY") * (sl."quantity") AS "Koszt urzadzen PLN"
-		,(sl."lineAmount") - ((sl."ednOryUnitCostLCY") * (sl."quantity")) AS "Zysk PLN"
-		,sl."ednCoolingCapacityKW" AS "Moc chłodnicza"
-		,CONCAT(qh."Firma", '_', qh."Sell_to_Customer_No") AS "Nr klienta"		-- Litera firmy dodana w elu utworzenia klucza klienta w każdej spółce
+		,((sl."lineAmount" * sl."ednPriceListExchangeRate") - (sl."ednOryUnitCostLCY") * (sl."quantity")) AS "Zysk PLN"
+		,sl."ednCoolingCapacityKW" AS "Moc chLodnicza"
+		,CONCAT(qh."Firma", '_', qh."Sell_to_Customer_No") AS "Klucz klienta"		-- Litera firmy dodana w elu utworzenia klucza klienta w każdej spółce
 		,qh."Sell_to_Customer_Name" AS "Nazwa klienta"
+--		,qh."VAT_Registration_No" AS "NIP"
 		,qh."Salesperson_Code" AS "Handlowiec"
 		,qh."Status" AS "Status oferty"
 		,qh."EDN_Quote_Type_Code" AS "Kod typu oferty"
@@ -61,22 +69,35 @@ Oferty_Technab AS (
 
 Oferty_Zymetric AS (
 	SELECT
-		CONCAT(sl."Firma", '_', sl."documentNo") AS "Klucz oferty"
-		,sl."documentType"
-		,sl."documentNo" AS "Numer oferty"
-		,sl."lineNo" AS "Linia oferty"
+		sl."documentNo" AS "Nr oferty"
+		,CONCAT(sl."Firma", '_', sl."documentNo") AS "Klucz oferty"
+		,sl."documentType" AS "Typ dokumentu"
 		,sl."shortcutDimension2Code" AS "Nr projektu"
 		,CONCAT(sl."Firma", '_', sl."shortcutDimension2Code") AS "Klucz projektu"
 		,qh."Document_Date" AS "Data dokumentu"
+		,sl."lineNo" AS "Linia oferty"
 		,sl."no" AS "Symbol urzadzenia"
-		,sl."lineAmount" AS "Wartosc PLN"
 		,sl."quantity" AS "Ilosc"
+		,sl."lineAmount" AS "Wartosc"
+		,qh."Currency_Code" as "Kod waluty dokumentu"
+		,sl."ednPriceListExchangeRate" AS "Kurs wymiany (do zmiany)"
+		,CASE
+			WHEN qh."Currency_Code" = '' THEN sl."lineAmount"
+			ELSE (sl."lineAmount" * sl."ednPriceListExchangeRate")
+		END AS "Wartosc PLN"
 --		,sl."ednOryUnitCostLCY" as "koszt"
 		,(sl."ednOryUnitCostLCY") * (sl."quantity") AS "Koszt urzadzen PLN"
-		,(sl."lineAmount") - ((sl."ednOryUnitCostLCY") * (sl."quantity")) AS "Zysk PLN"
-		,sl."ednCoolingCapacityKW" AS "Moc chłodnicza"
-		,CONCAT(qh."Firma", '_', qh."Sell_to_Customer_No") AS "Nr klienta"		-- Litera firmy dodana w elu utworzenia klucza klienta w każdej spółce
+		,(
+			CASE
+				WHEN qh."Currency_Code" = '' THEN sl."lineAmount"
+				ELSE (sl."lineAmount" * sl."ednPriceListExchangeRate")
+			END
+		) - (sl."ednOryUnitCostLCY") * (sl."quantity")
+			AS "Zysk PLN"
+		,sl."ednCoolingCapacityKW" AS "Moc chLodnicza"
+		,CONCAT(qh."Firma", '_', qh."Sell_to_Customer_No") AS "Klucz nabywcy"		-- Litera firmy dodana w elu utworzenia klucza klienta w każdej spółce
 		,qh."Sell_to_Customer_Name" AS "Nazwa klienta"
+--		,qh."VAT_Registration_No" AS "NIP"
 		,qh."Salesperson_Code" AS "Handlowiec"
 		,qh."Status" AS "Status oferty"
 		,qh."EDN_Quote_Type_Code" AS "Kod typu oferty"
