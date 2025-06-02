64de3e82f23723e4d259cc11ea9cfbd0766e0406
Woojek1 <wojciklukaszk@gmail.com>
2025-06-02 15:05:16 +0200

    Add Price Lists for all companies to Gold Layer

--- DWH/Scripts/ETL/Silver to Gold/bc_silver_to_gold_Price_Lists_with_MGL_availability.sql
+++ DWH/Scripts/ETL/Silver to Gold/bc_silver_to_gold_Price_Lists_with_MGL_availability.sql
@@ -1,27 +1,80 @@
 CREATE OR REPLACE VIEW v_bc_price_lists AS
 WITH Price_Lists_Aircon AS (
-	SELECT
-		pl."AssetNo"
+	select
+		pl."PriceListCode"
+		,pl."AssetNo"
 		,pl."UnitPrice"
-		,pl."PriceListCode"
 		,pl."StartingDate"
 		,pl."EndingDate"
 		,i."mkGLQuantity"
 		,pl."load_ts" AS "LoadDate"
-		,'Aircon' AS "Company"
-		
+		,'Aircon' AS "Company"		
 	FROM	
-		silver.bc_price_lists_aircom pl
+		silver.bc_price_lists_aircon pl
 	left join
 		bronze.bc_items_aircon i
 	on pl."AssetNo" = i."No"
 	where
 		pl."StartingDate" < CURRENT_DATE
 	and
-		pl."EndingDate" > CURRENT_DATE
+		(pl."EndingDate" = '0001-01-01' or pl."EndingDate" > CURRENT_DATE)
+),
 
+Price_Lists_Technab as (
+	SELECT
+		pl."PriceListCode"
+		,pl."AssetNo"
+		,pl."UnitPrice"
+		,pl."StartingDate"
+		,pl."EndingDate"
+		,i."mkGLQuantity"
+		,pl."load_ts" AS "LoadDate"
+		,'Technab' AS "Company"		
+	FROM	
+		silver.bc_price_lists_technab pl
+	left join
+		bronze.bc_items_technab i
+	on pl."AssetNo" = i."No"
+	where
+		pl."StartingDate" < CURRENT_DATE
+	and
+		(pl."EndingDate" = '0001-01-01' or pl."EndingDate" > CURRENT_DATE)
+),
 
+Price_Lists_Zymetric as (
+	SELECT
+		pl."PriceListCode"
+		,pl."AssetNo"
+		,pl."UnitPrice"
+		,pl."StartingDate"
+		,pl."EndingDate"
+		,i."mkGLQuantity"
+		,pl."load_ts" AS "LoadDate"
+		,'Zymetric' AS "Company"		
+	FROM	
+		silver.bc_price_lists_zymetric pl
+	left join
+		bronze.bc_items_zymetric i
+	on pl."AssetNo" = i."No"
+	where
+		pl."StartingDate" < CURRENT_DATE
+	and
+		(pl."EndingDate" = '0001-01-01' or pl."EndingDate" > CURRENT_DATE)
 )
+
+select *
+from
+	Price_Lists_Aircon
+union all
+
+select *
+from
+	Price_Lists_Technab
+union all
+
+select *
+from
+	Price_Lists_Aircon
 
 
 -------------------
