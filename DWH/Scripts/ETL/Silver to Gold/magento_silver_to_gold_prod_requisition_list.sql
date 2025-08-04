29472895c05fe8889d50e926a968b3b3e6791ab3
Łukasz <wojciklukaszk@gmail.com>
2025-08-04 10:13:39 +0200

    Add Magento Requisitiopn List to gold Layer

--- DWH/Scripts/ETL/Silver to Gold/magento_silver_to_gold_prod_requisition_list.sql
+++ DWH/Scripts/ETL/Silver to Gold/magento_silver_to_gold_prod_requisition_list.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE VIEW gold.v_magento_prod_reward AS
+CREATE OR REPLACE VIEW gold.v_magento_prod_requisition_list AS
 SELECT
-	reward_id
-	,customer_id
-	,website_id
-	,points_balance
-	,website_currency_code
-	,load_ts
+	"entity_id"
+	,"customer_id"
+	,"name"
+	,"description"
+	,"updated_at"
+	,"load_ts"
 FROM 
-	silver.magento_prod_reward
+	silver.magento_prod_requisition_list
