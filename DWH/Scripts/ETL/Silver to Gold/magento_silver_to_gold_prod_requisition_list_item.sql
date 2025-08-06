94c34d24da8adc3049cf2028bd150ac2a21ac768
Łukasz <wojciklukaszk@gmail.com>
2025-08-06 08:44:59 +0200

    Add Magento Requisition List Item to Silver Layer

--- DWH/Scripts/ETL/Silver to Gold/magento_silver_to_gold_prod_requisition_list_item.sql
+++ DWH/Scripts/ETL/Silver to Gold/magento_silver_to_gold_prod_requisition_list_item.sql
@@ -1,10 +1,12 @@
-CREATE OR REPLACE VIEW gold.v_magento_prod_requisition_list AS
+CREATE OR REPLACE VIEW gold.v_magento_prod_requisition_list_item AS
 SELECT
-	"entity_id"
-	,"customer_id"
-	,"name"
-	,"description"
-	,"updated_at"
-	,"load_ts"
+	item_id
+	,requisition_list_id
+	,sku
+	,store_id
+	,added_at
+	,qty
+	,"options"
+	,load_ts
 FROM 
-	silver.magento_prod_requisition_list
+	silver.magento_prod_requisition_list_item
