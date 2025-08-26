39fcd40bd131ec3b86e7770fde28b5d01f055ee7
Łukasz <wojciklukaszk@gmail.com>
2025-08-26 09:12:31 +0200

    add spare parts report to silver layer

--- DWH/Scripts/ETL/Silver to Gold/bc_nav_silver_to_gold_zapotrzebowanie_czesci.sql
+++ DWH/Scripts/ETL/Silver to Gold/bc_nav_silver_to_gold_zapotrzebowanie_czesci.sql
@@ -2,7 +2,7 @@ create or replace view gold.Zapotrzebowanie_na_czesci as
 
 with failure_table as (
 
-	select
+select
 		ROW_NUMBER() OVER (PARTITION BY fil."Item_No_Serviced", fil."Base_GroupB" ORDER BY fil."Time_To_Failure (month)") as "Numer_wiersza (i)"
 		,COUNT(*) OVER (PARTITION BY fil."Item_No_Serviced", fil."Base_GroupB") AS "Ilosc_awarii (n)"
 		,((ROW_NUMBER() OVER (PARTITION BY fil."Item_No_Serviced", fil."Base_GroupB" ORDER BY fil."Time_To_Failure (month)") - 0.3)::numeric) 
@@ -31,8 +31,7 @@ with failure_table as (
 		"Time_To_Failure (month)" > 1
 	and
 		"Item_No_Serviced" <> 'G-EF07UMX'
---	and
---		"Base_GroupB" = 'PCB_358'
+
 	)
 	
 	select
@@ -41,7 +40,7 @@ with failure_table as (
   		,regr_slope("Y", "X") as "a (wsp. kierunkowy)"
   		,regr_intercept("Y", "X") as "b (wyraz wolny)" 
   		,EXP(- regr_intercept("Y","X") / regr_slope("Y","X")) AS eta
-  		,max(sia."Quantity") as "Sold_quantity"
+--  	,max(sia."Quantity") as "Sold_quantity"
   		,max("Ilosc_awarii (n)") as "Ilosc_awarii"
 --  		,1-exp(
 --  			-power(2/(EXP(- regr_intercept("Y","X") / regr_slope("Y","X"))),
@@ -61,8 +60,8 @@ with failure_table as (
 	left join
 		gold."Dim_BC_NAV_Sold_Items_Aggregation" sia
 	on ft."Item_No_Serviced" = sia."Item_No"
-	where 
-		"Empiryczne prawdopodobienstwo_awarii (Ti)"	> 0
+--	where 
+--		"Empiryczne prawdopodobienstwo_awarii (Ti)"	> 0
 	group by
 		"Item_No_Serviced"
 		,"Base_GroupB"
@@ -70,7 +69,3 @@ with failure_table as (
 		max("Ilosc_awarii (n)") > 1
 	and
 		regr_slope("Y", "X") is not null
-
-
-
-	
