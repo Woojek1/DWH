1d2ca2be51341e5f34ca04d07eb87cccb571002e
Łukasz <wojciklukaszk@gmail.com>
2025-09-23 10:27:12 +0200

    Change email for hr enova tables

--- DWH/Scripts/ETL/Bronze to Silver/hr_enova_bronze_to_silver_Struktura_Organizacyjna.sql
+++ DWH/Scripts/ETL/Bronze to Silver/hr_enova_bronze_to_silver_Struktura_Organizacyjna.sql
@@ -154,7 +154,7 @@ EXECUTE format($etl$
 		,"load_ts"
 	)
 	SELECT 
-		$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20
+		$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19
   -- ilość musi odpowiadać ilości kolumn w tabeli docelowej
 	
 	ON CONFLICT("PracownikID") DO UPDATE
